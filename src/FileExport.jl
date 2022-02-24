ec79075455c4ac7ae4c5fc52ed299cb7e4199031
Konrad <konrad.scheffler@tuhh.de>
2022-02-24 12:16:24 +0100

    docu added

--- src/FileExport.jl
+++ src/FileExport.jl
@@ -12,6 +12,23 @@ function exportImage(filename, im::AbstractMatrix{T}, colorm::String;kargs...) w
   return exportImage(filename, im, cmap(colorm); kargs...)
 end
 
+"""
+    exportImage(filename, im, colorm; vmin, vmax, normalize, pixelResizeFactor)
+
+Exports image-data (twodimensional Colorant Array) `im` as `filename` (path) colored with the colormap `colorm` (default: grays).
+
+Colormaps can be given directly as a Vector of Colorants.
+Alternatively a colormap from ColorSchemes (with added alpha gradient from 0 to 1) can be used by giving a String with the name of the colormap.
+All existing colormaps can be found using `existing_cmaps()`.
+Frequently used colormaps can be found using `important_cmaps()`.
+Colormaps with customized alpha gradients can be build using `RGBAGradient()`.
+
+Further keyword arguments:
+- `vmin` and `vmax` (default: 0.0 and 1.0): floating numbers, left and right limits for coloring
+- `normalize` (default: true): boolean, normalize data to [0,1] if true
+- `pixelResizeFactor` (default: 1): integer number, amount of multiple pixels plotted for each datapoint
+
+"""
 function exportImage(filename, im::AbstractMatrix{T}, colorm::Vector{C}; vmin=0.0, vmax=1.0,
                     normalize=true, pixelResizeFactor=1) where {T<:Real, C<:Colorant}
   imC = colorize( im, vmin, vmax, colorm, normalize=normalize )
@@ -38,7 +55,6 @@ function exportImage(filename, data::Vector{T}; kargs...) where {T<:AbstractMatr
     exportImage(file*"_yz.png", data[3]; kargs...)
 end
 
-
 ### export movies ###
 
 exportMovie(filename, data::ImageMeta; kargs...) = exportMovie(filename, data.data; kargs...)
@@ -53,6 +69,23 @@ function exportMovie(filename, data::AbstractArray{T,3}, colorm::String; kargs..
   return exportMovie(filename, data, cmap(colorm); kargs...)
 end
 
+"""
+    exportMovie(filename, data, colorm; vmin, vmax, normalize, pixelResizeFactor)
+
+Exports video-data (threedimensional Colorant Array) `data` as `filename` (path) colored with the colormap `colorm` (default: grays).
+
+Colormaps can be given directly as a Vector of Colorants.
+Alternatively a colormap from ColorSchemes (with added alpha gradient from 0 to 1) can be used by giving a String with the name of the colormap.
+All existing colormaps can be found using `existing_cmaps()`.
+Frequently used colormaps can be found using `important_cmaps()`.
+Colormaps with customized alpha gradients can be build using `RGBAGradient()`.
+
+Further keyword arguments:
+- `vmin` and `vmax` (default: 0.0 and 1.0): floating numbers, left and right limits for coloring
+- `normalize` (default: true): boolean, normalize data to [0,1] if true
+- `pixelResizeFactor` (default: 1): integer number, amount of multiple pixels plotted for each datapoint
+
+"""
 function exportMovie(filename, data::AbstractArray{T,3}, colorm::Vector{C}; vmin=0.0, vmax=1.0,
                     normalize=true, pixelResizeFactor=1) where {T<:Real, C<:Colorant}
     imC = colorize( data, vmin, vmax, colorm, normalize=normalize )
